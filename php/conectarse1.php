d98df0df8807b80e6c51f36f96d4c17e975618ad
GusArboleda0510 <gustavo.arboleda@correounivalle.edu.co>
2021-03-30 19:46:35 -0500

    commitArboleda

--- php/conectarse1.php
+++ php/conectarse1.php
@@ -3,26 +3,4 @@
     function conexion(){
         return $conexion=mysqli_connect("localhost","root","","u700865171_suenointimo");
     }
-    /*$host ="localhost"; //Direccion IP o el Servido de MYSQL
-    $usuario ="u700865171_sunointimo"; //Nombre del usuario del Servidor de MYSQL
-    $contrasena = "Www201855712"; //Contrasena del Servido de MYSQL
-    $baseDatos ="u700865171_suenointimo";
-
-    function conectarse(){
-
-        global $host, $usuario, $contrasena, $baseDatos,$tabla;
-        if(!($link = mysqli_connect($host, $usuario, $contrasena))){
-            echo "ERROR CONECTANDO A LA BASE DE DATOS. <br>" ; 
-        }else{
-            echo "Listo, SE HA CONECTADO <br>" ;
-        }
-        if (!mysqli_select_db($link, $baseDatos)){
-            echo "ERROR CONECTANDO A LA BD <br>";
-        }else{
-            echo "EXITO AL CONECTANDO  $baseDatos A LA BD  <br>";
-        }
-        return $link;
-    }
-    $link = conectarse();
-    $result = mysqli_query ($link, $query);*/
 ?>
--- php/iniciarLogeo.php
+++ php/iniciarLogeo.php
@@ -20,35 +20,4 @@
         }else{
             echo "0";
         }
-/*
-    include 'conectarse1.php';
-    $query = "SELECT nombre, email, contrasena FROM usuario;";
-    $result = mysqli_query($link, $query);
-
-    $email = $_POST['emaillogeo1'];
-    $contrasena = password_hash($_POST['contraseñalogeo1'], PASSWORD_BCRYPT);
-    ?>
-    <br>
-    <?php
-    while ($row = mysqli_fetch_array($result)) {
-        //printf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td>",$row["codigo"],$row["nombre"],$row["email"],$row["contrasena"]);
-        if ($email == $row["email"]) {
-            echo "existe <br>";
-            if ($contrasena == $row["contrasena"]) {
-                echo "contraseña correcta <br>";
-                echo "inicio sesion <br>";
-                break;
-            } else {
-                echo "contraseña incorrecta <br>";
-                echo "$contrasena <br>";
-                echo $row["contrasena"];
-                break;
-            }
-        } else {
-            echo "no existe el usuario <br>";
-            break;
-        }
-    }
-    mysqli_free_result($result);
-    mysqli_close($link);*/
     ?>
