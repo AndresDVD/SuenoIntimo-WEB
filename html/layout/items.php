f07fdbf212bc84c23e23436b805f37248a57fbe3
CrazorDVD <andres12dvd@gmail.com>
2021-04-04 21:00:49 -0500

    commitAndres

--- html/layout/items.php
+++ html/layout/items.php
@@ -1,6 +1,6 @@
 <div class="articulo">
     <input type="hidden" id="id" value="<?php echo $item['id']; ?>">
-    <div class="imagen"><img src="../../imgs/items/<?php echo $item['imagen'];?>" alt=""></div>
+    <div class="imagen"><img src="../imgs/items/<?php echo $item['imagen'];?>"></div>
     <div class="titulo"><?php echo $item['nombre'];?></div>
     <div class="precio">$<?php echo $item['precio'];?></div>
     <div class="botones">
